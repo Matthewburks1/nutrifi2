27b8169923af977fd87ccd48691373034315782e
Matthewburks1 <124093747+Matthewburks1@users.noreply.github.com>
2023-04-03 14:10:46 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+require('dotenv-flow').config();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+require('./routes/middlewares/mongo');
+
+const app = express()
+const port = 3080
+
+app.use(morgan('dev'))
+app.use(cors())
+app.use((req, res, next) => {
+  if (req.originalUrl === '/api/stripe/webhook') {
+    next();
+  } else {
+    bodyParser.json()(req, res, next);
+  }
+});
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+app.set('jwt', "ebeb1a5ada5cf38bfc2b49ed5b3100e0");
+
+app.use('/api', require('./routes/api'))
+
+// Default Index Page
+app.use(express.static(__dirname + '/build'));
+// Send all other items to index file
+app.get('*', (req, res) => res.sendFile(__dirname + '/build/index.html'));
+
+app.listen(port, () => {
+  console.log(`Example app listening at ${process.env.DOMAIN}:${port}`)
+})
